edf5318d77ac2c7cdd192fa33fff7ce724006fa9
Victor <bvictor@ethz.ch>
2021-07-07 20:01:27 +0200

    lvl2

--- extract_all_habitats.jl
+++ extract_all_habitats.jl
@@ -1,5 +1,6 @@
 cd(@__DIR__)
 using Pkg; Pkg.activate(".")
+using PyPlot
 using ArchGDAL; const AG = ArchGDAL
 using Statistics
 using ProgressMeter
@@ -8,48 +9,51 @@ using Dates
 verbose = true
 test = false
 plotting = false
+simulation = true
 
 include("src/extract_graph_from_raster.jl")
-savedir = "results/lvl2_frac_1km_ver004"
+savedir = "results/lvl1_frac_1km_ver004"
 isdir(savedir) ? nothing : mkpath(savedir)
-## COMPOSITE ##
 
-datalist = glob("./data/lvl2_frac_1km_ver004/*.tif")
+if simulation
+    datalist = glob("./data/lvl2_frac_1km_ver004/*.tif")
+    println("Starting computation")
+    println(now())
+    Threads.@sync Threads.@threads for f in datalist
+        dataset = AG.read(f)
+        fsplit = split(f,"_")
+        window_size = 100#km
+        habitat = parse(UInt16,fsplit[9]) #lvl1 forest
+        newraster_i = 1:window_size:AG.width(dataset)
+        newraster_j = 1:window_size:AG.height(dataset)
+        newraster = zeros(length(newraster_i), length(newraster_j))
 
-println("Starting computation")
-println(now())
-Threads.@sync Threads.@threads for f in datalist
-    dataset = AG.read(f)
-    fsplit = split(f,"_")
-    window_size = 100#km
-    habitat = parse(UInt16,fsplit[9]) #lvl1 forest
-    newraster_i = 1:window_size:AG.width(dataset)
-    newraster_j = 1:window_size:AG.height(dataset)
-    newraster = zeros(length(newraster_i), length(newraster_j))
+        # preallocating
+        data = AG.read(dataset, 1, 1, 1, window_size, window_size) #x, y , window size
 
-    # preallocating
-    data = AG.read(dataset, 1, 1, 1, window_size, window_size) #x, y , window size
-
-    # main loop
-    for (ii,i) in enumerate(newraster_i[1:end-1]), (jj,j) in enumerate(newraster_j[1:end-1])
-        data .= AG.read(dataset, 1, i, j, window_size, window_size) #x, y , window size
-        ncells = count(data .== habitat)
-        if ncells > 0
-            verbose && println(ncells, " cells of habitats ", habitat, " were found")
-            g = extract_graph(data, habitat)
-            metrics = mean(degree(g))
-            verbose && println("mean metrics = ", metrics)
-            newraster[ii,jj] = metrics
-        end
-        if test
-            if i > 1
-                break
+        # main loop
+        for (ii,i) in enumerate(newraster_i[1:end-1]), (jj,j) in enumerate(newraster_j[1:end-1])
+            data .= AG.read(dataset, 1, i, j, window_size, window_size) #x, y , window size
+            ncells = count(data .== habitat)
+            if ncells > 0
+                verbose && println(ncells, " cells of habitats ", habitat, " were found")
+                g = extract_graph(data, habitat)
+                metrics = mean(degree(g))
+                verbose && println("mean metrics = ", metrics)
+                newraster[ii,jj] = metrics
+            end
+            if test
+                if i > 1
+                    break
+                end
             end
         end
+        savename = split(f,"/")[end]
+        savename = string(split(savename,".")[1],".jld2")
+        @save joinpath(savedir,savename) newraster
     end
-    savename = split(f,"/")[end]
-    savename = string(split(savename,".")[1],".jld2")
-    @save joinpath(savedir,savename) newraster
+    println(now())
+    println("Computation over")
 end
 
 # plotting
@@ -65,5 +69,3 @@ if plotting
         plt.close(fig)
     end
 end
-println(now())
-println("Computation over")
