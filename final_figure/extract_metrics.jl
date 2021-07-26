7f502ab9f67557616ec1699f620a40df63bb8e31
Victor <bvictor@ethz.ch>
2021-07-26 16:29:20 +0200

    final figure working

--- final_figure/extract_metrics.jl
+++ final_figure/extract_metrics.jl
@@ -3,8 +3,8 @@ raster_i = 1:window_size:size(myim,1)
 raster_j = 1:window_size:size(myim,2)
 
 raster = Dict(
-    "sqrtk" => zeros(length(raster_i), length(raster_j)),
-    # "assortativity" => zeros(length(raster_i), length(raster_j)),
+    "sqrtk" => fill(NaN,length(raster_i), length(raster_j)),
+    "assortativity" => fill(NaN,length(raster_i), length(raster_j)),
     )
 
 for ii in 1:length(raster_i)-1
@@ -13,18 +13,18 @@ for ii in 1:length(raster_i)-1
         datahab_ij = view(myim, i:i+window_size,j:j+window_size)
         ncells = count(datahab_ij .> 0)
         if ncells > 0
-            # datatemp_ij = view(data_temp, i:i+window_size,j:j+window_size)
+            datatemp_ij = view(mytemp, i:i+window_size,j:j+window_size)
             verbose && println(ncells, " cells of habitats ", habitat, " were found")
             g, B = extract_graph_1km(datahab_ij, area_threshold)
             # calculating metrics
             raster["sqrtk"][ii,jj] = sqrtk(g)
-            # raster["assortativity"][ii,jj] = assortativity(g, datatemp_ij[B])
+            raster["assortativity"][ii,jj] = assortativity(g, datatemp_ij[B])
             # storing graph
             # raster_g[ii,jj] = g
 
             if verbose
                 println("sqrtk = ", raster["sqrtk"][ii,jj])
-                # println("assortativity = ", raster["assortativity"][ii,jj])
+                println("assortativity = ", raster["assortativity"][ii,jj])
             end
         end
     end
--- final_figure/final_figure.jl
+++ final_figure/final_figure.jl
@@ -1,5 +1,6 @@
 # ENV["PYTHON"] = "/usr/local/anaconda3/envs/land2graph/bin/python"
 # using Pkg; Pkg.build("PyCall")
+import EvoId.eth_grad_std
 using PyCall
 ccrs = pyimport("cartopy.crs")
 cf = pyimport("cartopy.feature")
@@ -10,39 +11,46 @@ geometry = pyimport("shapely.geometry")
 cd(@__DIR__)
 rivers_50m = cf.NaturalEarthFeature("physical", "rivers_lake_centerlines", "50m")
 
-filename_hab = "../data/lvl1_frac_1km_ver004/iucn_habitatclassification_fraction_lvl1__100_Forest__ver004.tif"
+# filename_hab = "../data/lvl1_frac_1km_ver004/iucn_habitatclassification_fraction_lvl1__100_Forest__ver004.tif"
+# filename_hab = "/Users/victorboussange/ETHZ/projects/neutralmarker/Land2Graph/data/lvl2_frac_1km_ver004/iucn_habitatclassification_fraction_lvl2__104_Forest – Temperate__ver004.tif"
+filename_hab = "/Users/victorboussange/ETHZ/projects/neutralmarker/Land2Graph/data/lvl2_frac_1km_ver004/iucn_habitatclassification_fraction_lvl2__404_Grassland – Temperate__ver004.tif"
+area_threshold = 800
+
 filename_temp = "../data/chelsa/CHELSA_bio12_reprojected_nearest.tif"
 # calculate extent of raster
 xmin = 94.132175
 xmax = 101.873646
 ymin =  22.097833
-ymax = 31.7
+ymax = 31.834458
 
 # adding the window that we like
 windowminx = 99.
 windowmaxx = 99.15
-windowminy = 25
-windowmaxy = 25.15
+windowminy = 27.2
+windowmaxy = 27.35
 
 verbose = true
-habitat = "forest"
+habitat = "grassland"
 
-src = rasterio.open(filename_hab, "r")
+data_src = rasterio.open(filename_hab, "r")
+data_src_temp = rasterio.open(filename_temp, "r")
+
 # try
 # longitude / latitudes for top left and bottom right corner of hengduan window
-row, col = src.index(xmin, ymin)
-row2, col2 = src.index(xmax, ymax)
+row, col = data_src.index(xmin, ymin)
+row2, col2 = data_src.index(xmax, ymax)
 # read image into ndarray
-global myim = src.read(1, window = rasterio.windows.Window( col, row2, col2-col, row - row2))
+global myim = data_src.read(1, window = rasterio.windows.Window( col, row2, col2-col, row - row2))
+global mytemp = data_src_temp.read(1, window = rasterio.windows.Window( col, row2, col2-col, row - row2))/10. .- 273.15
 
 # longitude / latitudes for top left and bottom right corner of hengduan window
-global row, col = src.index(windowminx, windowminy)
-global windowminx, windowminy = src.xy(row, col)
-global row2, col2 = src.index(windowmaxx, windowmaxy)
-global windowmaxx, windowmaxy = src.xy(row2, col2)
-global myim2 = src.read(1, window = rasterio.windows.Window( col, row2, col2-col, row - row2))
+global row, col = data_src.index(windowminx, windowminy)
+global windowminx, windowminy = data_src.xy(row, col)
+global row2, col2 = data_src.index(windowmaxx, windowmaxy)
+global windowmaxx, windowmaxy = data_src.xy(row2, col2)
+global myim2 = data_src.read(1, window = rasterio.windows.Window( col, row2, col2-col, row - row2))
 # finally 
-#     src.close()
+#     data_src.close()
 # end
 
 
@@ -51,7 +59,10 @@ crs = ccrs.PlateCarree()
 
 
 # create figure
-fig = plt.figure()
+fig = plt.figure(
+            # constrained_layout=true,
+            figsize = FIGSIZE_L
+            )
 gs = fig.add_gridspec(2, 2)
 ax1 = fig.add_subplot(py"$gs[0, 0]", projection=crs)
 ax2 = fig.add_subplot(py"$gs[1, 0]", projection=crs)
@@ -67,7 +78,13 @@ ax1.set_xmargin(0.05)
 ax1.set_ymargin(0.10)
 
 # plot raster
-ax1.imshow(myim, extent=[xmin, xmax, ymin, ymax], transform=crs, interpolation="nearest")
+pos1 = ax1.imshow(myim, 
+                extent=[xmin, xmax, ymin, ymax], 
+                transform=crs, 
+                interpolation="nearest", 
+                cmap = :RdBu)
+
+cb = plt.colorbar(pos1, ax=ax1, label = "Habitat prop", shrink=0.5)
 gl = ax1.gridlines(draw_labels=true)
 gl.xlabels_top = false
 gl.ylabels_right = false
@@ -93,7 +110,8 @@ ax2.set_ymargin(0.10)
 [i.set_color("r") for i in ax2.spines.values()]
 
 # plot raster
-ax2.imshow(myim2, origin="upper", extent=[windowminx - src.transform[1]/2, windowmaxx - src.transform[1]/2, windowminy - src.transform[5]/2, windowmaxy - src.transform[5]/2], transform=crs)
+pos2 = ax2.imshow(myim2, origin="upper", extent=[windowminx - data_src.transform[1]/2, windowmaxx - data_src.transform[1]/2, windowminy - data_src.transform[5]/2, windowmaxy - data_src.transform[5]/2], transform=crs)
+# cb = colorbar(pos2, ax=ax2, label = "habitat")
 
 # plot features
 ax2.coastlines(resolution="auto", color="red")
@@ -109,7 +127,6 @@ gcf()
 include("../src/extract_graph_from_raster.jl")
 include("../src/graph_metrics.jl")
 include("../../graphs_utils/src/graphs_utils.jl")
-area_threshold = 900
 
 g, B = extract_graph_1km(myim2, area_threshold)
 coord_graph = CartesianIndices(myim2)[B]
@@ -117,13 +134,13 @@ gx = to_nx(g)
 
 pos = Dict{Int,Array}()
 for i in 1:length(coord_graph)
-    xs, ys = src.xy(row2 + coord_graph[i][1] - 1, col + coord_graph[i][2] - 1, offset="center")
+    xs, ys = data_src.xy(row2 + coord_graph[i][1] - 1, col + coord_graph[i][2] - 1, offset="center")
     pos[i-1] = [xs[], ys[]]
 end
 nx.draw_networkx_nodes(gx,
                 pos,
                 edgecolors="tab:blue",
-                node_size = 1e0,
+                node_size = 1e1,
                 node_color = "tab:blue",
                 # horizontalalignment = "right",
                 # verticalalignment = "baseline",
@@ -143,11 +160,30 @@ gl.ylines = false
 
 ax2.margins(0.10)
 ################################
-######### ax 3##################
+######### ax 3 / 4##################
 ################################
 include("extract_metrics.jl")
 
-ax3.imshow(raster["sqrtk"], extent=[xmin, xmax, ymin, ymax], transform=crs, interpolation="nearest")
-gcf()
+pos3 = ax3.imshow(raster["sqrtk"], extent=[xmin, xmax, ymin, ymax], transform=crs, interpolation="nearest")
+cb = plt.colorbar(pos3, ax=ax3, label = "Sqrtk", shrink=0.5)
+gl = ax3.gridlines(draw_labels=true)
+gl.xlabels_top = false
+gl.ylabels_right = false
+gl.xlines = false
+gl.ylines = false
 
-# rasterio.transform.xy
+pos4 = ax4.imshow(raster["assortativity"], extent=[xmin, xmax, ymin, ymax], transform=crs, interpolation="nearest")
+cb = plt.colorbar(pos4, ax=ax4, label = "Assortativity", shrink=0.5)
+gl = ax4.gridlines(draw_labels=true)
+gl.xlabels_top = false
+gl.ylabels_right = false
+gl.xlines = false
+gl.ylines = false
+
+fig.tight_layout()
+fig.savefig("final_figure_habitatmapping_$habitat.pdf",
+            dpi=1200,
+            bbox_inches = "tight",
+            )
+
+gcf()
