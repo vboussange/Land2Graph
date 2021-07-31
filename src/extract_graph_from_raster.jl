f0f9b83b38d510e8e3613051b537811e01ce7341
Victor <bvictor@ethz.ch>
2021-07-31 17:00:47 +0200

    removed all scripts

--- src/extract_graph_from_raster.jl
+++ src/extract_graph_from_raster.jl
@@ -1,17 +1,16 @@
 using LightGraphs
 
 """
-    function extract_adjacency_from_raster(N::BitMatrix)
+    function extract_adjacency(N::BitMatrix)
 Given `N` a matrix of suitable habitat (`true` if suitable habitat at index `i,j`), returns a graph.
 Habitats are potentially connected to eight neighbours (left, diagonal left top, top, diagonal right top, etc...).
 """
-function extract_adjacency_from_raster(N)
+function extract_adjacency(N)
     s1 = size(N,1)
     s2 = size(N,2)
     _li = LinearIndices(N)
     A = zeros(Bool,length(N), length(N))
     for i in 1:s1, j in 1:s2
-        # @show i, j
         if N[i,j] # cell is not empty
             if i == s1 && j == s2
                 break
@@ -41,7 +40,7 @@ Extract graph from raster `N`, with habitat defined by value `habitat`
 """
 function extract_graph(N, habitat = 1)
     B = N .== habitat
-    A = extract_adjacency_from_raster(B)
+    A = extract_adjacency(B)
     g = SimpleGraph{Int16}(A)
     return g[LinearIndices(B)[B]]
 end
@@ -53,7 +52,7 @@ return graph and coordinate BitMatrix for habitats
 """
 function extract_graph_1km(N, area = 0)
     B = N .> area
-    A = extract_adjacency_from_raster(B)
+    A = extract_adjacency(B)
     g = SimpleGraph{Int16}(A)
     return g[LinearIndices(B)[B]], B
 end
